df09373d79a397578bdf5abc0bd87469c745b950
Adriano Borges Silva <120127965+AdrianoBorgesSilva@users.noreply.github.com>
2023-04-26 18:05:02 -0300

    Update

--- script.js
+++ script.js
@@ -1,22 +1,36 @@
 const vanGogh = document.getElementById('vanGogh_box')
 const monet = document.getElementById('monet_box')
 const rembrandt = document.getElementById('rembrandt_box')
+const vanGoghInfo = document.getElementById('vanGoghInfo')
+const monetInfo = document.getElementById('monetInfo')
+const rembrandtInfo = document.getElementById('rembrandtInfo')
+const infoContainer = [...document.querySelectorAll('.info_container')]
 
 vanGogh.addEventListener('click', ()=>{
     vanGogh.classList.toggle('select')
     monet.classList.toggle('deselect')
     rembrandt.classList.toggle('deselect')
+    vanGoghInfo.classList.toggle('show_info')
 })
 monet.addEventListener('click', ()=>{
     vanGogh.classList.toggle('deselect')
     monet.classList.toggle('select')
     rembrandt.classList.toggle('deselect')
+    monetInfo.classList.toggle('show_info')
 })
 rembrandt.addEventListener('click', ()=>{
     vanGogh.classList.toggle('deselect')
     monet.classList.toggle('deselect')
     rembrandt.classList.toggle('select')
+    rembrandtInfo.classList.toggle('show_info')
 })
+
+infoContainer.forEach((el)=>{
+    el.addEventListener('click', (evt)=>{
+        evt.stopPropagation()
+    })
+})
+
 
 
 
