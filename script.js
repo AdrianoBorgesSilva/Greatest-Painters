8dab15b059db9eb71323130ede299a4355205c38
Adriano Borges Silva <120127965+AdrianoBorgesSilva@users.noreply.github.com>
2023-04-25 21:11:40 -0300

    Update

---
+++ script.js
@@ -0,0 +1,25 @@
+const vanGogh = document.getElementById('vanGogh_box')
+const monet = document.getElementById('monet_box')
+const rembrandt = document.getElementById('rembrandt_box')
+const rbtn_gogh = document.getElementById('rbtn_gogh')
+
+vanGogh.addEventListener('click', ()=>{
+    vanGogh.classList.toggle('select')
+    monet.classList.toggle('deselect')
+    rembrandt.classList.toggle('deselect')
+})
+monet.addEventListener('click', ()=>{
+    vanGogh.classList.toggle('deselect')
+    monet.classList.toggle('select')
+    rembrandt.classList.toggle('deselect')
+})
+rembrandt.addEventListener('click', ()=>{
+    vanGogh.classList.toggle('deselect')
+    monet.classList.toggle('deselect')
+    rembrandt.classList.toggle('select')
+})
+
+
+
+
+
